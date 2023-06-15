2e3ac76ded9d428f14943db6a19e7bbe342b1d00
fillytewe1 <135631067+fillytewe1@users.noreply.github.com>
2023-06-15 18:08:35 +0200

    Add files via upload
    Here is how you estimate the time : 
    
    
    1 d    --> 1 Day 
    1h   ---> 1 Hour
    1m  --> 1 Minute
    23 -----> 23 Minute ( Minutes As well)
    
    So: 
    1d 1h 30m   ---> 25.5h
    
    
    You can also copy the input. My manager used it so he can insert it to our projectmanagemant software Easy Redmine.

---
+++ zeitschaetzungstool/Zeitschätzung/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Zeitschätzung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Beschreibung = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GeschaetzteZeit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbnSumme = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnloeschen = new System.Windows.Forms.Button();
+            this.lbnliveAusgabe = new System.Windows.Forms.Label();
+            this.lbnVollstaendig = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Beschreibung,
+            this.GeschaetzteZeit});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 5);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(720, 473);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellLeave);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
+            // 
+            // Beschreibung
+            // 
+            this.Beschreibung.FillWeight = 172.5889F;
+            this.Beschreibung.HeaderText = "Beschreibung";
+            this.Beschreibung.Name = "Beschreibung";
+            // 
+            // GeschaetzteZeit
+            // 
+            this.GeschaetzteZeit.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.GeschaetzteZeit.FillWeight = 27.41118F;
+            this.GeschaetzteZeit.HeaderText = "Geschätzte Zeit";
+            this.GeschaetzteZeit.Name = "GeschaetzteZeit";
+            // 
+            // lbnSumme
+            // 
+            this.lbnSumme.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbnSumme.AutoSize = true;
+            this.lbnSumme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbnSumme.Location = new System.Drawing.Point(455, 16);
+            this.lbnSumme.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbnSumme.Name = "lbnSumme";
+            this.lbnSumme.Size = new System.Drawing.Size(70, 18);
+            this.lbnSumme.TabIndex = 2;
+            this.lbnSumme.Text = "Summe:";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExport.Location = new System.Drawing.Point(0, 12);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(93, 27);
+            this.btnExport.TabIndex = 6;
+            this.btnExport.Text = "Kopieren";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnloeschen
+            // 
+            this.btnloeschen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnloeschen.Location = new System.Drawing.Point(109, 12);
+            this.btnloeschen.Margin = new System.Windows.Forms.Padding(2);
+            this.btnloeschen.Name = "btnloeschen";
+            this.btnloeschen.Size = new System.Drawing.Size(102, 27);
+            this.btnloeschen.TabIndex = 7;
+            this.btnloeschen.Text = "Zurücksetzen";
+            this.btnloeschen.UseVisualStyleBackColor = true;
+            this.btnloeschen.Click += new System.EventHandler(this.btnloeschen_Click);
+            // 
+            // lbnliveAusgabe
+            // 
+            this.lbnliveAusgabe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbnliveAusgabe.AutoSize = true;
+            this.lbnliveAusgabe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbnliveAusgabe.Location = new System.Drawing.Point(529, 16);
+            this.lbnliveAusgabe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbnliveAusgabe.Name = "lbnliveAusgabe";
+            this.lbnliveAusgabe.Size = new System.Drawing.Size(72, 18);
+            this.lbnliveAusgabe.TabIndex = 9;
+            this.lbnliveAusgabe.Text = "Ausgabe";
+            // 
+            // lbnVollstaendig
+            // 
+            this.lbnVollstaendig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbnVollstaendig.AutoSize = true;
+            this.lbnVollstaendig.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbnVollstaendig.Location = new System.Drawing.Point(307, 16);
+            this.lbnVollstaendig.Name = "lbnVollstaendig";
+            this.lbnVollstaendig.Size = new System.Drawing.Size(0, 18);
+            this.lbnVollstaendig.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbnSumme);
+            this.panel1.Controls.Add(this.lbnVollstaendig);
+            this.panel1.Controls.Add(this.btnExport);
+            this.panel1.Controls.Add(this.lbnliveAusgabe);
+            this.panel1.Controls.Add(this.btnloeschen);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(5, 427);
+            this.panel1.MinimumSize = new System.Drawing.Size(650, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(8);
+            this.panel1.Size = new System.Drawing.Size(720, 51);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 483);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Zeitschätzung ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbnSumme;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnloeschen;
+        private System.Windows.Forms.Label lbnliveAusgabe;
+        private System.Windows.Forms.Label lbnVollstaendig;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Beschreibung;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GeschaetzteZeit;
+    }
+}
+
---
+++ zeitschaetzungstool/Zeitschätzung/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zeitschätzung
+{
+    public partial class Form1 : Form
+    {
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+
+            dataGridView1.Columns[0].Width = 450;
+            dataGridView1.Columns[1].Width = 137;
+            dataGridView1.AllowUserToResizeColumns = true;
+            dataGridView1.AutoSize = true;
+            this.MinimumSize = new Size(675, 522);
+            /*dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 4; */
+            this.dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        { 
+            if (e.ColumnIndex == 1)
+            {
+                SummeAktualisieren();
+            }
+        }
+
+        public void SummeAktualisieren()
+        {
+            double summe = 0;
+
+            List<string> zeiten = new List<string>(); // liste eingabe 
+            for (int z = 0; z < dataGridView1.Rows.Count - 1; z++)
+            {
+                var aktuelleZeile = dataGridView1.Rows[z].Cells["GeschaetzteZeit"].Value;
+
+                if (aktuelleZeile != null)
+                {
+                    string[] bloecke = aktuelleZeile.ToString().Split(' ');
+
+                    foreach (string block in bloecke)
+                    {
+                        zeiten.Add(block.ToLower());
+                    }
+                }  // null ist ein nicht definiertes objekt // Funktionalität oder Eigenschaft für ein bestimmtes object
+            }
+
+            foreach (var zeit in zeiten)
+            {
+                try
+                {
+                    if (zeit.Contains("d")) // regex ist bessser string d = "d$"
+                    {
+
+                        summe += ErhalteStundenVonTag(zeit);
+                    }
+                    else if (zeit.Contains("h"))
+                    {
+                        summe += ErhalteStunden(zeit);
+                    }
+                    else
+                    {
+                        summe += ErhalteStundenVonMinuten(zeit);
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+
+            lbnliveAusgabe.Text = String.Format(summe % 1 == 0 ? "{0:0}" : "{0:0.00}", summe) + " Stunden";
+        }
+
+        private bool SindDatenVollstaendig()
+        {
+            for (int i = 0; i < this.dataGridView1.Rows.Count - 1; i++)
+            {
+                bool istBeschreibungVollstaendig = !String.IsNullOrWhiteSpace(dataGridView1.Rows[i].Cells[0].Value?.ToString());
+                bool istZeitVollstaendig = !String.IsNullOrWhiteSpace(dataGridView1.Rows[i].Cells[1].Value?.ToString());
+
+                if (!istBeschreibungVollstaendig || !istZeitVollstaendig)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IstZeileUnvollstaendig(DataGridViewRow row)
+        {
+            bool istBeschreibungUnvollstaendig = String.IsNullOrWhiteSpace(row.Cells[0].Value?.ToString());
+            bool istZeitUnvollstaendig = String.IsNullOrWhiteSpace(row.Cells[1].Value?.ToString());
+            return istBeschreibungUnvollstaendig || istZeitUnvollstaendig;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+
+            if (!SindDatenVollstaendig())
+            {
+                MessageBox.Show("Bitte füllen Sie alle Zellen aus ein bevor sie Kopieren.");
+                return;
+            }
+            else
+            {
+                var newline = System.Environment.NewLine;
+                var tab = "\t";
+                var clipboard_string = "";
+                var clipboard_stringToRedmineBeschreibungsWert = "";
+                var clipboard_stringToRedmineZahlenWert = "";
+                var namen = "||Beschreibung||Geschätzte Zeit|| \r\n";
+                
+                var htmlTableStart =
+                  @"<figure class=""table"">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <strong>Beschreibung</strong> 
+                            </td>
+                            <td>
+                                <strong>Geschätzte Zeit</strong> 
+                            </td>
+                        </tr>";
+                var htmlEnd =
+                    @"      </tbody>
+                         </table>
+                   </figure>";
+                 
+                var htmlTdOpenWithUserValue = "<td>"  ;
+                var htmlTdClose = "</td>"  ;
+
+                var htmlTrOpenAfterRow = "<tr>"  ;
+                var htmlTrCloseAfterRow = "</tr>"  ;
+                // DataGridViewCell testval;
+                //DataGridViewCell dddddd = new DataGridViewCell();
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    for (int i = 0; i < row.Cells.Count; i++)
+                    {
+
+                        if (IstZeileUnvollstaendig(row))
+                        {
+                            continue;
+                        }
+                        else if (i == (row.Cells.Count - 1))
+                        {
+                            //dddddd = (DataGridViewCell)row.Cells[i].Value;
+                            //testval += row.Cells[i].Value;
+                            clipboard_string += row.Cells[i].Value + "| " + newline;
+                            clipboard_stringToRedmineZahlenWert = htmlTdOpenWithUserValue 
+                                + row.Cells[i].Value   +
+                                htmlTdClose 
+                                + htmlTrCloseAfterRow;
+                            clipboard_stringToRedmineBeschreibungsWert += clipboard_stringToRedmineZahlenWert;
+                        }
+                        else
+                        {
+                            //testval += row.Cells[i].Value;
+                            clipboard_string += row.Cells[i].Value + tab;
+                            var temp= htmlTrOpenAfterRow + 
+                                htmlTdOpenWithUserValue + 
+                                row.Cells[i].Value
+                                + htmlTdClose  ;
+                                
+                            clipboard_stringToRedmineBeschreibungsWert += temp;
+                        }
+                    }
+                }
+                var zusammen = namen + clipboard_string;
+                var summeBlock = $@"<tr><td>Summe</td><td>{lbnliveAusgabe.Text.ToString()}</td></tr>";
+                var zusammenRedmine = htmlTableStart + clipboard_stringToRedmineBeschreibungsWert  + summeBlock + htmlEnd;
+                var formatiertRedmine = System.Xml.Linq.XElement.Parse(zusammenRedmine).ToString();
+                string[] splitClipboard = zusammen.Split('\t');
+                string combine = string.Join("|", splitClipboard);
+                string[] splitClipboardBuchstabeN = combine.Split('\n');
+                string secondcombine = string.Join("|", splitClipboardBuchstabeN);
+                string lastblock = secondcombine + " Summe " + "  | " + lbnliveAusgabe.Text.ToString() + "|";
+                // Clipboard.SetText(lastblock); The old code for Jira
+                //Clipboard.SetText(formatiertRedmine); // new Code for Redmine
+                 
+                this.dataGridView1.AllowUserToAddRows = false;
+                this.dataGridView1.Rows.Add("Summe", lbnliveAusgabe.Text.ToString());
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.SelectAll();
+                Clipboard.SetDataObject(
+                this.dataGridView1.GetClipboardContent());
+                int lastrow = dataGridView1.Rows.Count-1; 
+                dataGridView1.Rows.RemoveAt(lastrow);
+                dataGridView1.ClearSelection();
+                dataGridView1.RowHeadersVisible = true;
+                this.dataGridView1.AllowUserToAddRows = true;
+                
+            }
+
+        }
+
+        private double ErhalteStundenVonTag(string eingabe)
+        {
+            string regex = @"(\d+|\d+[,.]{1}\d+)d$";
+            var match = Regex.Match(eingabe, regex);
+
+            if (match.Success)
+            {
+                string zahl = match.Groups[1].Value.Replace(',', '.');
+                return Convert.ToDouble(zahl, new NumberFormatInfo() { NumberDecimalSeparator = "." }) * 8;
+            }
+            throw new Exception($"Ihre Eingabe '{ eingabe }' ist falsch!!");
+        }
+
+        private double ErhalteStundenVonMinuten(string eingabe)
+        {
+            string regex = @"(\d+)m$|^(\d+)$";
+            var match = Regex.Match(eingabe, regex);
+
+            if (match.Success)
+            {
+                string match1 = match.Groups[1].Value;
+                string match2 = match.Groups[2].Value;
+                if (string.IsNullOrWhiteSpace(match1))
+                {
+                    return Convert.ToDouble(match2) / 60;
+                }
+                else
+                {
+                    return Convert.ToDouble(match1) / 60;
+                }
+            }
+            throw new Exception($"Ihre Eingabe '{ eingabe }' ist falsch!!");
+        }
+
+        private double ErhalteStunden(string eingabe)
+        {
+            string regex = @"(\d+|\d+[,.]{1}\d+)h$";
+            var match = Regex.Match(eingabe, regex);
+
+            if (match.Success)
+            {
+                string zahl = match.Groups[1].Value.Replace(',', '.');
+                return Convert.ToDouble(zahl, new NumberFormatInfo() { NumberDecimalSeparator = "." });
+            }
+
+            throw new Exception($"Ihre Eingabe '{ eingabe }' ist falsch!!");
+        }
+
+        private void btnloeschen_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            lbnVollstaendig.Text = "";
+            lbnliveAusgabe.Text = "Ausgabe";
+        }
+
+        public static int totalRowcount(DataGridView dt)
+        {
+            int counter = 0;
+            foreach (DataGridViewRow row in dt.Rows)
+            {
+                counter = dt.Rows.Count;
+            }
+            return counter;
+        }
+
+        private void dataGridView1_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            bool isValid = true;
+
+            for (int rowIndex = 0; rowIndex < dataGridView1.Rows.Count -1 && isValid; rowIndex++)
+            {
+                var currentRow = dataGridView1.Rows[rowIndex];
+
+                for (int cellIndex = 0; cellIndex < dataGridView1.Rows[rowIndex].Cells.Count; cellIndex++)
+                {
+                    if (currentRow.Cells[cellIndex].Value == null || String.IsNullOrWhiteSpace(currentRow.Cells[cellIndex].Value.ToString()))
+                    {
+                        isValid = false;
+                    }
+                    else
+                    {
+                        lbnVollstaendig.Text = "";
+                    }
+                }
+            }
+            if (!isValid)
+            {
+                lbnVollstaendig.ForeColor = Color.Red;
+                lbnVollstaendig.Text = "Unvollständig";
+            }
+            else
+            {
+                lbnVollstaendig.Text = "";
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int indexcount = totalRowcount(dataGridView1);
+
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                int index = row.Index + 1;
+                if (indexcount == index)
+                {
+                    continue;
+                }
+                try
+                { 
+                    dataGridView1.Rows.Remove(row); 
+                }
+                catch
+                {
+
+                }
+            }
+
+            SummeAktualisieren();
+            
+        }
+    }
+}
+
+
+
